fe4c6896c8ccc51071d31d175e7a1482067bc7e1
EDGAR LOPEZ SOSA <16171409@itculiacan.edu.mx>
2022-02-22 15:22:15 -0700

    Agregando Boton

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -4,12 +4,32 @@ class HomeScreen extends StatelessWidget{
   
   const HomeScreen({Key? key}) : super(key: key);
 
-  
   @override
   Widget build(BuildContext context) {
-    return const Center(
-        child: Text("HomeScreen"),
-      );
+
+    const fontSize25 = TextStyle(fontSize: 25);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(child: Text('HomeScreen AppBar')),
+        elevation: 5,
+      ),
+      body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const <Widget>[
+              Text("Click Counter:", style: fontSize25),
+              Text('10', style: fontSize25)
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.plus_one_outlined) ,
+          onPressed: () { 
+
+          },
+        ),
+    );
   }
 
 }
